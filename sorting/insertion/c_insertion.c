b08f056836fd0a5619f9fd2f0519a7a3611569da
Luke Gilson <luke.gilson@gmail.com>
2016-09-29 13:35:25 -0400

    updated for current scripts

--- sorting/insertion/c_insertion.c
+++ sorting/insertion/c_insertion.c
@@ -1,36 +1,29 @@
 #include <stdio.h>
+#include <time.h>
 
-void insertion_sort(int A[], int a_length);
-void print_array(int A[], int a_length);
+float wrapper(long A[], size_t a_length);
+void insertion_sort(long A[], size_t a_length);
 
 int main() {
-	
-	int array[] = {5,2,6,0,1};
-	print_array(array, 5);
-	insertion_sort(array, 5);
-	print_array(array, 5);
-	
-	
 
 }
 
-void insertion_sort(int A[], int a_length) {
+float wrapper(long A[], size_t a_length) {
+    clock_t start = clock();
+    insertion_sort(A, a_length);
+    clock_t end = clock();
+    float time_spent = (float)(end - start) / CLOCKS_PER_SEC;
+    return time_spent;
+}
+
+void insertion_sort(long A[], size_t a_length) {
 	signed int val;
 	for (unsigned int indx = 1; indx < a_length; indx++) {
 		val = A[indx];
 		int lft_indx = indx - 1;
-		
+
 		for (; lft_indx >= 0 && A[lft_indx] > val; lft_indx--)
 			A[lft_indx + 1] = A[lft_indx];
 		A[lft_indx + 1] = val;
 	}
-}
-
-void print_array(int A[], int a_length) {
-	printf("\n");
-
-	for (int i = 0; i < a_length; i++)
-		printf("%d, ",A[i]);
-	
-	printf("\n");
 }
--- sorting/insertion/python_insertion.py
+++ sorting/insertion/python_insertion.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python
+import time
+
+def wrapper(A, a_length):
+    start = time.clock()
+    insertion_sort(A)
+    end = time.clock()
+
+    return end - start
 
 def insertion_sort(A):
-    
+
     for indx in range(1, len(A)):
         val = A[indx]
         lft_indx = indx - 1
-        
+
         while lft_indx >= 0 and A[lft_indx] > val:
             A[lft_indx + 1] = A[lft_indx]
             lft_indx -= 1
         A[lft_indx + 1] = val
-        
+
     return A
 
 if __name__ == '__main__':
-    
+    ''' quick test '''
     A = [1,4,2,7,4,45,23]
     print insertion_sort(A)
